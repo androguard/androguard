9ba083c2b3b7aeb8097e9993077ff7ce6d85b1e7
pouik <pouik@camelot>
2010-11-19 11:50:08 +0100

    comment lupa

--- core/wm/wm_l3.py
+++ core/wm/wm_l3.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Androguard.  If not, see <http://www.gnu.org/licenses/>.
 
-import lupa
+#import lupa
 
 lua_code_init = '''
       %s = {}
