151f7abca1611afb85809cc5b9c5d021cc2a3236
Anthony Desnos <desnos@t0t0.fr>
2010-12-13 16:05:27 +0100

    fix dvm bug

--- demos/androguard_test_ANALYSIS.py
+++ demos/androguard_test_ANALYSIS.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import sys
+import sys, hashlib
+
 PATH_INSTALL = "./"                                                                                                                                                                                                               
 sys.path.append(PATH_INSTALL + "./")
 
@@ -10,7 +11,8 @@ OUTPUT = "./output/"
 #TEST  = 'examples/java/test/orig/Test1.class'
 #TEST  = 'examples/java/Demo1/orig/DES.class'
 #TEST  = 'examples/java/Demo1/orig/Util.class'
-TEST = "examples/android/Test/bin/classes.dex"
+#TEST = 'examples/android/Test/bin/classes.dex'
+TEST = 'examples/android/Hello_Kitty/classes.dex'
 
 a = androguard.AndroguardS( TEST )
 x = analysis.VM_BCA( a.get_vm() )
@@ -20,8 +22,8 @@ x = analysis.VM_BCA( a.get_vm() )
 for method in a.get_methods() :
    g = x.hmethods[ method ]
    
-   g.basic_blocks.export_dot( OUTPUT + method.get_name() + ".dot" )
+   g.basic_blocks.export_dot( OUTPUT + "%s-%s" % (method.get_name(), hashlib.md5( "%s-%s" % (method.get_class_name(), method.get_descriptor())).hexdigest()) + ".dot" )
 
-   print method.get_name()
+   print method.get_class_name(), method.get_name(), method.get_descriptor()
    for i in g.basic_blocks.get() :
-      print "\t", i.name, i.start, i.end, '[ CHILDS = ', ', '.join( j[2].get_name() for j in i.childs ), ']', '[ FATHERS = ', ', '.join( j[2].get_name() for j in i.fathers ), ']', i.free_blocks_offsets
+      print "\t %s %x %x" % (i.name, i.start, i.end), '[ CHILDS = ', ', '.join( "%x-%x-%s" % (j[0], j[1], j[2].get_name()) for j in i.childs ), ']', '[ FATHERS = ', ', '.join( j[2].get_name() for j in i.fathers ), ']', i.free_blocks_offsets
