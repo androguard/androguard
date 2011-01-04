72faa79bbfe46a6019afd10b42078fde879f8a2f
Anthony Desnos <desnos@t0t0.fr>
2011-01-04 09:23:07 +0100

    add disassembler example

---
+++ demos/disassembler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+
+PATH_INSTALL = "./"                                                                                                                                                                                                               
+sys.path.append(PATH_INSTALL + "./")
+
+import androguard, analysis
+
+TEST  = 'examples/java/test/orig/Test1.class'
+#TEST = 'examples/android/Test/bin/classes.dex'
+
+a = androguard.AndroguardS( TEST )
+x = analysis.VM_BCA( a.get_vm() )
+
+for method in a.get_methods() :
+   print method.get_class_name(), method.get_name(), method.get_descriptor()
+   code = method.get_code()
+   bc = code.get_bc()
+
+   idx = 0
+   for i in bc.get() :
+      print "\t", "%x" % idx, i.get_name(), i.get_operands()
+
+      idx += i.get_length()
