5fda4620b2fa2202196bb582c19ac8d2915acad1
pouik <pouik@camelot>
2011-03-02 09:55:50 +0100

    add androaxml

--- demos/axml_format_1.py
+++ demos/axml_format_1.py
@@ -6,14 +6,9 @@ PATH_INSTALL = "./"
 sys.path.append(PATH_INSTALL + "/core")
 sys.path.append(PATH_INSTALL + "/core/bytecodes")
 
+from xml.dom import minidom
 import dvm
 
-TEST = "./examples/android/Test/bin/Test-debug.apk"
-#TEST = "examples/android/Hello_Kitty/Hello_Kitty_Wallpapers_3.0.0.apk"
-
-#ap = dvm.AXMLPrinter( open("/tmp/AndroidManifest.xml", "r").read() )
 ap = dvm.AXMLPrinter( open("apks/tmp/AndroidManifest.xml", "r").read() )
 
-fd = open("TotoManifest.xml", "w")
-fd.write( ap.getBuff() )
-fd.close()
+print minidom.parseString( ap.getBuff() ).toxml()
