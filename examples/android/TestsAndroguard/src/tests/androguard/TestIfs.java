1e5ab65b7bc945efc50b336183421d839e79f7d6
zost <Lircyn@users.noreply.github.com>
2011-09-23 09:19:43 +0200

    update decompiler

--- examples/android/TestsAndroguard/src/tests/androguard/TestIfs.java
+++ examples/android/TestsAndroguard/src/tests/androguard/TestIfs.java
@@ -1,8 +1,9 @@
 package tests.androguard;
 
 public class TestIfs {
+	private boolean P, Q, R, S, T;
 
-	public static int IF( int p ) {
+	public static int testIF( int p ) {
 		int i;
 		
 		if ( p > 0 ) {
@@ -13,7 +14,7 @@ public class TestIfs {
 		return i;
 	}
 	
-	public static int IF2( int p ) {
+	public static int testIF2( int p ) {
 		int i = 0;
 		
 		if ( p > 0 ) {
@@ -22,5 +23,46 @@ public class TestIfs {
 			i = p + 2;
 		}
 		return i;
+	}
+	
+	public static int testShortCircuit( int p ) {
+		int i = 0;
+		if ( p > 0 && p % 2 == 3 ) {
+			i = p + 1;
+		} else {
+			i = -p;
+		}
+		return i;
+	}
+	
+	public static int testShortCircuit2( int p ) {
+		int i = 0;
+		if ( p <= 0 || p % 2 != 3 )
+			i = -p;
+		else
+			i = p + 1;
+		return i;
+	}
+	
+	public void testCFG( ) {
+		int I = 1, J = 1, K = 1, L = 1;
+		
+		do {
+			if ( P ) {
+				J = I;
+				if ( Q )
+					L = 2;
+				else L = 3;
+				K++;
+			} else {
+				K += 2;
+			}
+			System.out.println(I + "," + J + "," + K + "," + L);
+			do {
+				if ( R )
+					L += 4;
+			} while ( !S );
+			I += 6;
+		} while ( !T );
 	}
 }
--- examples/android/TestsAndroguard/src/tests/androguard/TestLoops.java
+++ examples/android/TestsAndroguard/src/tests/androguard/TestLoops.java
@@ -8,7 +8,7 @@ public class TestLoops {
 		public static int j;
 	}
 	
-	public void aWhile( ) {
+	public void testWhile( ) {
 		int i = 5, j = 10;
 		while( i < j ) {
 			j += i / 2.0 + j;
@@ -18,7 +18,7 @@ public class TestLoops {
 		Loop.j = j;
 	}
 
-	public void aFor( ) {
+	public void testFor( ) {
 		int i, j;
 		for( i = 5, j = 10; i < j; i += i * 2 ) {
 			j += i / 2.0 + j;
@@ -27,7 +27,7 @@ public class TestLoops {
 		Loop.j = j;
 	}
 	
-	public void aDoWhile( ) {
+	public void testDoWhile( ) {
 		int i = 5, j = 10;
 		do
 		{
@@ -37,4 +37,65 @@ public class TestLoops {
 		Loop.i = i;
 		Loop.j = j;
 	}
+	
+	public int testNestedLoops( int a )
+	{
+		if ( a > 1000 ) {
+			return testNestedLoops( a / 2 );
+		}
+		else {
+			while( a > 0 ) {
+				a += 1;
+				while ( a % 2 == 0 ) {
+					a *= 2;
+					while ( a % 3 == 0 ) {
+						a -= 3;
+					}
+				}
+			}
+		}
+		return a;
+	}
+	
+	public void testMultipleLoops( )
+	{
+		int a = 0;
+		while ( a < 50 )
+			a += 2;
+		while ( a % 3 == 0 )
+			a *= 5;
+		while ( a < 789 && a > 901 )
+			System.out.println("woo");
+	}
+
+    public int testDoWhileTrue( int n )
+    {
+        do {
+            n--;
+            if ( n == 2 )
+                return 5;
+            if ( n < 2 )
+                n = 500;
+        } while( true );
+    }
+
+    public int testWhileTrue( int n )
+    {
+        while ( true ) {
+            n--;
+            if ( n == 2 )
+                return 5;
+            if ( n < 2 )
+                n = 500;
+        }
+    }
+
+    public int testDiffWhileDoWhile( int n )
+    {
+        while ( n != 2 ) {
+            if ( n < 2 )
+                n = 500;
+        }
+        return 5;
+    }
 }
