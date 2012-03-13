d184f95259c5754deccf5b70d52ec92fbefd249c
Anthony Desnos <desnos@t0t0.fr>
2012-03-13 19:16:52 +0100

    fix signature bug

--- ant/test_ant.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#./ant/androguard_ant.py ./examples/android/Test/bin/classes/org/t0t0/android/ ./examples/android/Test/androguard.xml
-
-./ant/androguard_ant.py ./examples/java/TC/orig/:./examples/java/TC/orig_main/ ./examples/java/TC/androguard.xml
-
-exit 0
