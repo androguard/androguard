cb8129328078d8c299ceb3d50db47b2f99d57af6
Anthony Desnos <desnos@t0t0.fr>
2012-03-27 15:36:57 +0200

    update setup

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name = 'androguard',
     version = '1.0',
     packages = find_packages(),
-    scripts = ['androaxml.py'],
+    scripts = ['androaxml.py', 'androcsign.py', 'androdiff.py', 'androgexf.py',
+        'androlyze.py', 'andromercury.py', 'androrisk.py', 'androsign.py',
+        'androsim.py', 'androxgmml.py', 'apkviewer.py'],
     install_requires=['distribute'],
 )
