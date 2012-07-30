c8363a445b2c18e203ffe9f23256b96774731a6e
Anthony Desnos <desnos@t0t0.fr>
2012-07-30 23:07:20 +0200

    up example for zost noob !;)

--- demos/disassembler_special_ins.py
+++ demos/disassembler_special_ins.py
@@ -23,8 +23,16 @@ for method in a.get_methods() :
     for i in g.basic_blocks.get() :
         for ins in i.get_instructions() :   
             op_value = ins.get_op_value()
+            
+            # packed/sparse
             if op_value == 0x2b or op_value == 0x2c :
                 special_ins = i.get_special_ins(idx)
                 if special_ins != None :
                     print "\t %x" % idx, ins, special_ins, ins.get_name(), ins.get_output(), special_ins.get_values()
+            # fill
+            if op_value == 0x26 :
+                special_ins = i.get_special_ins(idx)
+                if special_ins != None :
+                    print "\t %x" % idx, ins, special_ins, ins.get_name(), ins.get_output(), repr( special_ins.get_data() )
+
             idx += ins.get_length()
