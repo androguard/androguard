b43062afde58be42064597e0104304fce62a0197
bl4ckh0l3z <bl4ckh0l3z@gmail.com>
2015-04-13 11:50:52 +0200

    Fix for is_android_raw in detecting *.apk files
    The previous version of 'is_android_raw' is not able to detect some *.apk files in which the string 'PK' is not in the first block of 7 bytes. 
    So, to correctly detect all *.apk files we need also to check some immutable resources which are absolutely present in their packages, such as 'AndroidManifest.xml' and 'META-INF/MANIFEST.MF'.

--- androguard/core/androconf.py
+++ androguard/core/androconf.py
@@ -209,26 +209,25 @@ def is_android(filename):
 
     val = None
     with open(filename, "r") as fd:
-        f_bytes = fd.read(7)
+        f_bytes = fd.read()
         val = is_android_raw( f_bytes )
 
     return val
 
 def is_android_raw(raw):
     val = None
-    f_bytes = raw[:7]
 
-    if f_bytes[0:2] == "PK":
+    if raw[0:2] == "PK" or ('AndroidManifest.xml' in raw and 'META-INF/MANIFEST.MF' in raw):
         val = "APK"
-    elif f_bytes[0:3] == "dex":
+    elif raw[0:3] == "dex":
         val = "DEX"
-    elif f_bytes[0:3] == "dey":
+    elif raw[0:3] == "dey":
         val = "DEY"
-    elif f_bytes[0:7] == "\x7fELF\x01\x01\x01":
+    elif raw[0:7] == "\x7fELF\x01\x01\x01":
         val = "ELF"
-    elif f_bytes[0:4] == "\x03\x00\x08\x00":
+    elif raw[0:4] == "\x03\x00\x08\x00":
         val = "AXML"
-    elif f_bytes[0:4] == "\x02\x00\x0C\x00":
+    elif raw[0:4] == "\x02\x00\x0C\x00":
         val = "ARSC"
 
     return val
