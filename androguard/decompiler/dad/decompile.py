64cc59f0ba0574e70120c24483a81e2bd423f411
Lircyn <Lircyn@users.noreply.github.com>
2015-05-22 01:26:40 +0200

    .

--- androguard/decompiler/dad/decompile.py
+++ androguard/decompiler/dad/decompile.py
@@ -248,7 +248,11 @@ class DvClass(object):
         for klass in self.subclasses.values():
             klass.process(doAST=doAST)
         for i in range(len(self.methods)):
-            self.process_method(i, doAST=doAST)
+            try:
+                self.process_method(i, doAST=doAST)
+            except Exception as e:
+                logger.debug(
+                    'Error decompiling method %s: %s', self.methods[i], e)
 
     def get_ast(self):
         fields = [get_field_ast(f) for f in self.fields]
