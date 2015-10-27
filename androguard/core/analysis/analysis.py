68f85399217369b88b6bcb1235a01900b4b2fb33
adesnos <desnos@t0t0.fr>
2015-10-27 15:14:00 +0100

    update

--- androguard/core/analysis/analysis.py
+++ androguard/core/analysis/analysis.py
@@ -698,7 +698,7 @@ class newVMAnalysis(object):
         self.strings = {}
 
         for current_class in vm.get_classes():
-            self.classes[current_class.get_name()] = (
+            self.classes[current_class.get_name()] = ClassAnalysis(
                 current_class)
 
     def create_xref(self):
