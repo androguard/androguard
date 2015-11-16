46355eda22c7684711ab02671101359dc65a9a62
dhilipsiva <dhilipsiva@gmail.com>
2015-11-16 16:31:46 +0530

    More python 3 conversions

--- androguard/core/androconf.py
+++ androguard/core/androconf.py
@@ -177,13 +177,13 @@ def long2int(l):
 def long2str(l):
     """Convert an integer to a string."""
     if type(l) not in (types.IntType, types.LongType):
-        raise ValueError, 'the input must be an integer'
+        raise ValueError('the input must be an integer')
 
     if l < 0:
-        raise ValueError, 'the input must be greater than 0'
+        raise ValueError('the input must be greater than 0')
     s = ''
     while l:
-        s = s + chr(l & 255L)
+        s = s + chr(l & 255)
         l >>= 8
 
     return s
@@ -191,9 +191,9 @@ def long2str(l):
 def str2long(s):
     """Convert a string to a long integer."""
     if type(s) not in (types.StringType, types.UnicodeType):
-        raise ValueError, 'the input must be a string'
+        raise ValueError('the input must be a string')
 
-    l = 0L
+    l = 0
     for i in s:
         l <<= 8
         l |= ord(i)
--- androguard/core/bytecode.py
+++ androguard/core/bytecode.py
@@ -21,7 +21,7 @@ from struct import unpack, pack
 import textwrap
 
 import json
-from androconf import warning, error, CONF, enable_colors, remove_colors, save_colors, color_range
+from androguard.core.androconf import warning, error, CONF, enable_colors, remove_colors, save_colors, color_range
 
 
 def disable_print_colors():
