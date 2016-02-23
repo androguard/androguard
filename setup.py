ed593a2fd7ebb9959546bdac1ffbdcccfa346427
dhilipsiva <dhilipsiva@gmail.com>
2016-02-23 08:39:58 +0530

    Removing pypandoc

--- setup.py
+++ setup.py
@@ -14,25 +14,11 @@ Date created: 2015-11-24
 """
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-f = path.join(here, 'README.md')
-
-try:
-    from pypandoc import convert
-    long_description = convert(f, 'rst')
-except ImportError:
-    print(
-        "pypandoc module not found, could not convert Markdown to RST")
-    long_description = open(f, 'r').read()
 
 setup(
     name='ak-androguard',
-    version='3.1',
+    version='3.2',
     description="A fork of official Androguard project",
-    long_description=long_description,
     url='https://github.com/appknox/ak-androguard',
     author='dhilipsiva',
     author_email='dhilipsiva@gmail.com',
