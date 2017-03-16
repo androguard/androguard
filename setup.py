027edcaaa9e286a20d05cc0d3a6e0ed2b438b4a7
adesnos <desnos@t0t0.fr>
2017-03-16 11:15:24 +0100

    Fix errors

--- setup.py
+++ setup.py
@@ -8,11 +8,7 @@ setup(
     version='3.0',
     packages=find_packages(),
     scripts=['androaxml.py',
-             'androcsign.py',
-             'androdiff.py',
              'androlyze.py',
-             'androsign.py',
-             'androsim.py',
              'androdd.py',
              'androgui.py',],
     install_requires=['distribute'],)
