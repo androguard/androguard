73344fd217608d8efb74285a127333b370c78ee9
Sebastian Bachmann <me@free-minds.net>
2017-06-23 17:25:05 +0200

    more test cases

--- tests/test_decompiler.py
+++ tests/test_decompiler.py
@@ -6,8 +6,6 @@ import sys
 PATH_INSTALL = "./"
 sys.path.append(PATH_INSTALL)
 
-from androguard.core.bytecodes import apk
-
 
 class DecompilerTest(unittest.TestCase):
     def testSimplification(self):
@@ -16,6 +14,16 @@ class DecompilerTest(unittest.TestCase):
         z, = d.get_classes()
 
         self.assertIn("return ((23 - p3) | ((p3 + 66) & 26));", z.get_source())
+
+    def testArrays(self):
+        h, d, dx = AnalyzeDex("examples/tests/FillArrays.dex")
+
+        z, = d.get_classes()
+
+        self.assertIn("{20, 30, 40, 50};", z.get_source())
+        self.assertIn("{1, 2, 3, 4, 5, 999, 10324234};", z.get_source())
+        self.assertIn("{97, 98, 120, 122, 99};", z.get_source())
+        self.assertIn("{5, 10, 15, 20};", z.get_source())
 
 if __name__ == '__main__':
     unittest.main()
