8ce6c8330499a2a8cc58aeeb2778837f623c8c5b
Sebastian Bachmann <me@free-minds.net>
2017-06-23 17:27:27 +0200

    also test the wrong bits

--- tests/test_decompiler.py
+++ tests/test_decompiler.py
@@ -24,6 +24,10 @@ class DecompilerTest(unittest.TestCase):
         self.assertIn("{1, 2, 3, 4, 5, 999, 10324234};", z.get_source())
         self.assertIn("{97, 98, 120, 122, 99};", z.get_source())
         self.assertIn("{5, 10, 15, 20};", z.get_source())
+        # Failed version of char array
+        self.assertNotIn("{97, 0, 98, 0, 120};", z.get_source())
+        # Failed version of short array
+        self.assertNotIn("{5, 0, 10, 0};", z.get_source())
 
 if __name__ == '__main__':
     unittest.main()
