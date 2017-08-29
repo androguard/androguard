faa6e59e61cd437337827c3e822439ba4b07e5c4
Sebastian Bachmann <bachmann.s@ikarus.at>
2017-08-29 13:35:20 +0200

    adding more useful unit tests

--- tests/test_axml.py
+++ tests/test_axml.py
@@ -22,7 +22,7 @@ class AXMLTest(unittest.TestCase):
         for filename in filenames:
             with open(filename, "rb") as fd:
                 ap = apk.AXMLPrinter(fd.read())
-                self.assertTrue(ap)
+                self.assertIsNotNone(ap)
 
     def testNonZeroStyleOffset(self):
         """
@@ -36,7 +36,7 @@ class AXMLTest(unittest.TestCase):
 
         # Try to load in etree
         e = etree.fromstring(ap.get_buff())
-        self.assertTrue(e)
+        self.assertIsNotNone(e)
 
     def testExtraNamespace(self):
         """
@@ -49,8 +49,17 @@ class AXMLTest(unittest.TestCase):
 
         # Try to load in etree
         e = etree.fromstring(ap.get_buff())
-        self.assertTrue(e)
+        self.assertIsNotNone(e)
 
+    def testExtraNamespace(self):
+        """
+        Assert that files with a broken filesize are not parsed
+        """
+        filename = "examples/axml/AndroidManifestWrongFilesize.xml"
+
+        with self.assertRaises(AssertionError) as cnx:
+            apk.AXMLPrinter(open(filename, "rb").read())
+        self.assertTrue("Declared filesize does not match" in str(cnx.exception))
 
 
 if __name__ == '__main__':
