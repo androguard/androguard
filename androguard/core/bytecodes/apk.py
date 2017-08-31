e1eab2339fd0a5cd18cc95419665bd9b8263009d
Sebastian Bachmann <bachmann.s@ikarus.at>
2017-08-31 11:12:39 +0200

    fix for python 2.7

--- androguard/core/bytecodes/apk.py
+++ androguard/core/bytecodes/apk.py
@@ -806,11 +806,13 @@ class APK(object):
         #                   The length of to read bytes is then coded
         #                   in byte1 & 0x7F
         # Check if the first byte is 0xA0 (Sequence Tag)
-        if cert[0] == 0xA0:
-            l = cert[1]
-            # Python2 compliance
-            if not isinstance(l, int):
-                l = ord(l)
+        tag = cert[0]
+        l = cert[1]
+        # Python2 compliance
+        if not isinstance(l, int):
+            l = ord(l)
+            tag = ord(tag)
+        if tag == 0xA0:
             cert = cert[2 + (l & 0x7F) if l & 0x80 > 1 else 2:]
 
         certificate = x509.load_der_x509_certificate(cert, default_backend())
