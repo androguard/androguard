5b3cfb6fcde427d275cd8ef2e87432b9a27f2ecf
Sebastian Bachmann <me@free-minds.net>
2017-09-08 12:16:49 +0200

    lets add another testcase for this

--- tests/test_decompiler.py
+++ tests/test_decompiler.py
@@ -2,6 +2,8 @@ import unittest
 
 from androguard.misc import AnalyzeDex
 import sys
+from androguard.misc import AnalyzeAPK
+from androguard.decompiler.dad.decompile import DvMethod
 
 PATH_INSTALL = "./"
 sys.path.append(PATH_INSTALL)
@@ -28,6 +30,22 @@ class DecompilerTest(unittest.TestCase):
         self.assertNotIn("{97, 0, 98, 0, 120};", z.get_source())
         # Failed version of short array
         self.assertNotIn("{5, 0, 10, 0};", z.get_source())
+
+    def testProcessMethods(self):
+        """
+        Test if processing of methods works fine.
+        This can take some time!
+        """
+        a,d,dx = AnalyzeAPK("examples/tests/hello-world.apk")
+
+        for c in d.get_classes():
+            for m in d.get_methods():
+                mx = dx.get_method(m)
+                ms = DvMethod(mx)
+                ms.process()
+
+                self.assertIsNotNone(ms.get_source())
+
 
 if __name__ == '__main__':
     unittest.main()
