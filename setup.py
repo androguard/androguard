a0fab2b2fc2f7acaa1a4cf705baa90b6fdfb879d
Sebastian Bachmann <me@free-minds.net>
2017-12-20 12:37:12 +0100

    adding nose timer to see what tests are incredibly slow

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         'magic': ['file-magic'],
         'docs': ['sphinx', sphinxprogram, 'sphinx_rtd_theme'],
         'graphing': ['pydot'],
-	'tests': ['mock>=2.0', 'nose', 'codecov', 'coverage'],
+        'tests': ['mock>=2.0', 'nose', 'codecov', 'coverage', 'nose-timer'],
     },
     setup_requires=['setuptools'],
 
