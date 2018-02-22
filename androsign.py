01d8ce0f72a4732cfd643734f9017bdf6fdc50b2
Sebastian Bachmann <bachmann.s@ikarus.at>
2018-02-22 09:46:25 +0100

    allow errors

--- androsign.py
+++ androsign.py
@@ -5,6 +5,9 @@ from argparse import ArgumentParser
 import sys
 import os
 import hashlib
+import traceback
+from colorama import Fore, Back, Style
+
 
 def get_parser():
     parser = ArgumentParser(description="Return the fingerprint(s) of all certificates inside an APK")
@@ -12,6 +15,8 @@ def get_parser():
     parser.add_argument("apk", nargs="+", help="APK(s) to extract the Fingerprint of Certificates from")
     parser.add_argument("--hash", default="sha1", help="Fingerprint Hash algorithm, default SHA1")
     parser.add_argument("--all", "-a", default=False, action="store_true", help="Print all supported hashes")
+
+    return parser
 
 def main():
     parser = get_parser()
@@ -30,24 +35,29 @@ def main():
         sys.exit(1)
 
     for path in args.apk:
-        a = APK(path)
+        try:
+            a = APK(path)
 
-        print("{}, package: '{}'".format(os.path.basename(path), a.get_package()))
-        print("Is signed v1: {}".format(a.is_signed_v1()))
-        print("Is signed v2: {}".format(a.is_signed_v2()))
+            print("{}, package: '{}'".format(os.path.basename(path), a.get_package()))
+            print("Is signed v1: {}".format(a.is_signed_v1()))
+            print("Is signed v2: {}".format(a.is_signed_v2()))
 
-        certs = set(a.get_certificates_der_v2() + [a.get_certificate_der(x) for x in a.get_signature_names()])
+            certs = set(a.get_certificates_der_v2() + [a.get_certificate_der(x) for x in a.get_signature_names()])
 
-        if len(certs) > 0:
-            print("Found {} unique certificates".format(len(certs)))
+            if len(certs) > 0:
+                print("Found {} unique certificates".format(len(certs)))
 
-        for cert in certs:
+            for cert in certs:
 
-            if not args.all:
-                print("{} {}".format(args.hash.lower(), hashfunctions[args.hash.lower()](cert).hexdigest()))
-            else:
-                for k, v in hashfunctions.items():
-                    print("{} {}".format(k, v(cert).hexdigest()))
+                if not args.all:
+                    print("{} {}".format(args.hash.lower(), hashfunctions[args.hash.lower()](cert).hexdigest()))
+                else:
+                    for k, v in hashfunctions.items():
+                        print("{} {}".format(k, v(cert).hexdigest()))
+        except:
+            print(Fore.RED + "Error in {}".format(os.path.basename(path)) + Style.RESET_ALL, file=sys.stderr)
+            traceback.print_exc(file=sys.stderr)
+
         if len(args.apk) > 1:
             print()
 
