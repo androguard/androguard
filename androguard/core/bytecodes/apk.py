e49f3cafd6b5f16ef19a96872a06efb6f6371dd5
Sebastian Bachmann <me@free-minds.net>
2018-03-15 18:39:48 +0100

    remove alias

--- androguard/core/bytecodes/apk.py
+++ androguard/core/bytecodes/apk.py
@@ -1,8 +1,6 @@
 from __future__ import division
 from __future__ import print_function
-from future import standard_library
 
-standard_library.install_aliases()
 from builtins import str
 from builtins import object
 from androguard.core import androconf
