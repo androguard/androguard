ddaa672981c94817dc9be6a25c22d8ee55dc6ea6
Sebastian Bachmann <me@free-minds.net>
2018-04-26 20:51:24 +0200

    fix

--- androguard/misc.py
+++ androguard/misc.py
@@ -71,7 +71,7 @@ def AnalyzeAPK(_file, session=None, raw=False):
             df = DalvikVMFormat(dex, using_api=a.get_target_sdk_version())
             dx.add(df)
             d.append(df)
-            df.set_decompiler(DecompilerDAD(d, dx))
+            df.set_decompiler(decompiler.DecompilerDAD(d, dx))
 
         dx.create_xref()
 
