9e55c204ba862b2b918b41c245a1834aa33e74b9
shuxin <shuxin@users.noreply.github.com>
2018-04-30 08:30:09 +0800

    switch pyasn1 to asn1crypto
    so many bugs in pyasn1. asn1crypto is more easy in use, and no need cryptography any more.

--- androguard/util.py
+++ androguard/util.py
@@ -21,15 +21,29 @@ def get_certificate_name_string(name, short=False):
 
     # For the shortform, we have a lookup table
     # See RFC4514 for more details
-    sf = {
-        "countryName": "C",
-        "stateOrProvinceName": "ST",
-        "localityName": "L",
-        "organizationalUnitName": "OU",
-        "organizationName": "O",
-        "commonName": "CN",
-        "emailAddress": "E",
+    _ = {
+        'business_category' : ("businessCategory", "businessCategory"),
+        'serial_number' : ("serialNumber", "serialNumber"),
+        'country_name' : ("C", "countryName"),
+        'postal_code' : ("postalCode", "postalCode"),
+        'state_or_province_name' : ("ST", "stateOrProvinceName"),
+        'locality_name' : ("L", "localityName"),
+        'street_address' : ("street", "streetAddress"),
+        'organization_name' : ("O", "organizationName"),
+        'organizational_unit_name' : ("OU", "organizationalUnitName"),
+        'title' : ("title", "title"),
+        'common_name' : ("CN", "commonName"),
+        'initials' : ("initials", "initials"),
+        'generation_qualifier' : ("generationQualifier", "generationQualifier"),
+        'surname' : ("SN", "surname"),
+        'given_name' : ("GN", "givenName"),
+        'name' : ("name", "name"),
+        'pseudonym' : ("pseudonym", "pseudonym"),
+        'dn_qualifier' : ("dnQualifier", "dnQualifier"),
+        'telephone_number' : ("telephoneNumber", "telephoneNumber"),
+        'email_address' : ("E", "emailAddress"),
+        'domain_component' : ("DC", "domainComponent"),
+        'name_distinguisher' : ("nameDistinguisher", "nameDistinguisher"),
+        'organization_identifier' : ("organizationIdentifier", "organizationIdentifier"),
     }
-    return ", ".join(
-        ["{}={}".format(attr.oid._name if not short or attr.oid._name not in sf else sf[attr.oid._name], attr.value) for
-         attr in name])
+    return ", ".join(["{}={}".format(_.get(attr,(attr,attr))[0 if short else 1], name[attr]) for attr in name])
