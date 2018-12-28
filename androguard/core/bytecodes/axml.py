4ae03538e07e006288a861b041fcfba70534f944
Sebastian Bachmann <bachmann.s@ikarus.at>
2018-12-28 12:48:18 +0100

    remove old code

--- androguard/core/bytecodes/axml.py
+++ androguard/core/bytecodes/axml.py
@@ -690,22 +690,26 @@ def format_value(_type, _data, lookup_string=lambda ix: "<string>"):
     return "<0x%X, type 0x%02X>" % (_data, _type)
 
 
-class AXMLETreePrinter:
+class AXMLPrinter:
     def __init__(self, raw_buff):
+        """
+        Converter for AXML Files into a XML string
+
+        A Reference Implementation can be found at http://androidxref.com/9.0.0_r3/xref/frameworks/base/tools/aapt/XMLNode.cpp
+
+        :param raw_buff: bytes of the raw AXML file
+        """
         self.axml = AXMLParser(raw_buff)
 
         self.root = None
         cur = []
-        depth = 0
 
         while self.axml.is_valid():
             _type = next(self.axml)
 
             if _type == START_DOCUMENT:
-                pass
+                continue
             if _type == START_TAG:
-                depth += 1
-
                 name = self.axml.getName()
                 uri = self.axml.sb.getString(self.axml.m_namespaceUri)
                 if uri != "":
@@ -720,37 +724,45 @@ class AXMLETreePrinter:
                     if uri != "":
                         uri = "{{{}}}".format(uri)
                     name = self.axml.getAttributeName(i)
-                    value = self.getAttributeValue(i)
+                    value = self._get_attribute_value(i)
 
-                    log.debug("{} -- {} -- {}".format(uri, name, value))
+                    log.debug("found an attribute: {}{}='{}'".format(uri, name, value))
                     elem.set("{}{}".format(uri, name), value)
 
                 if self.root is None:
                     self.root = elem
                 else:
+                    if not cur:
+                        # looks like we lost the root?
+                        log.error("No more elements available to attach to! Is the XML malformed?")
+                        break
                     cur[-1].append(elem)
                 cur.append(elem)
 
             if _type == END_TAG:
-                depth -= 1
-                if depth < 0:
-                    log.warning("Too many END_TAG! depth is less than zero! depth={}".format(depth))
+                if not cur:
+                    log.warning("Too many END_TAG! No more elements available to attach to!")
                 cur.pop()
             if _type == TEXT:
+                log.debug("TEXT for {}".format(cur[-1]))
                 cur[-1].text = self.axml.getText()
             if _type == END_DOCUMENT:
                 break
 
     def get_buff(self):
-        return ""
+        """
+        Returns the raw XML file
+        :return: bytes, encoded as UTF-8
+        """
+        return self.get_xml(pretty=False)
 
-    def get_xml(self):
+    def get_xml(self, pretty=True):
         """
         Get the XML as an UTF-8 string
 
-        :return: str
+        :return: bytes encoded as UTF-8
         """
-        return etree.tostring(self.root, encoding="utf-8", pretty_print=True)
+        return etree.tostring(self.root, encoding="utf-8", pretty_print=pretty)
 
     def get_xml_obj(self):
         """
@@ -760,125 +772,12 @@ class AXMLETreePrinter:
         """
         return self.root
 
-    def getAttributeValue(self, index):
+    def _get_attribute_value(self, index):
         """
         Wrapper function for format_value
         to resolve the actual value of an attribute in a tag
-        :param index:
-        :return:
-        """
-        _type = self.axml.getAttributeValueType(index)
-        _data = self.axml.getAttributeValueData(index)
-
-        return format_value(_type, _data, lambda _: self.axml.getAttributeValue(index))
-
-
-class AXMLPrinter(object):
-    """
-    Converter for AXML Files into a XML string
-
-    A Reference Implementation can be found at http://androidxref.com/9.0.0_r3/xref/frameworks/base/tools/aapt/XMLNode.cpp
-    """
-    def __init__(self, raw_buff):
-        self.axml = AXMLParser(raw_buff)
-        self.xmlns = False
-
-        self.buff = u''
-
-        depth = 0
-
-        while self.axml.is_valid():
-            _type = next(self.axml)
-
-            if _type == START_DOCUMENT:
-                self.buff += u'<?xml version="1.0" encoding="utf-8"?>\n'
-            elif _type == START_TAG:
-                depth += 1
-                self.buff += u'<' + self.getPrefix(self.axml.getPrefix()) + self.axml.getName() + u'\n'
-                self.buff += self.axml.getXMLNS()
-
-                for i in range(0, self.axml.getAttributeCount()):
-                    prefix = self.getPrefix(self.axml.getAttributePrefix(i))
-                    name = self.axml.getAttributeName(i)
-                    value = self._escape(self.getAttributeValue(i))
-
-                    # If the name is a system name AND the prefix is set, we have a problem.
-                    # FIXME we are not sure how this happens, but a quick fix is to remove the prefix if it already in the name
-                    if name.startswith(prefix):
-                        prefix = u''
-
-                    self.buff += u'{}{}="{}"\n'.format(prefix, name, value)
-
-                self.buff += u'>\n'
-
-            elif _type == END_TAG:
-                depth -= 1
-                if depth < 0:
-                    log.warning("Too many END_TAG! Depth is less than zero! depth={}".format(depth))
-                self.buff += u"</%s%s>\n" % (
-                    self.getPrefix(self.axml.getPrefix()), self.axml.getName())
-
-            elif _type == TEXT:
-                self.buff += u"%s\n" % self._escape(self.axml.getText())
-            elif _type == END_DOCUMENT:
-                break
-
-    # pleed patch
-    # FIXME should this be applied for strings directly?
-    def _escape(self, s):
-        # FIXME Strings might contain null bytes. Should they be removed?
-        # We guess so, as normaly the string would terminate there...?!
-        s = s.replace("\x00", "")
-        # Other HTML Conversions
-        s = s.replace("&", "&amp;")
-        s = s.replace('"', "&quot;")
-        s = s.replace("'", "&apos;")
-        s = s.replace("<", "&lt;")
-        s = s.replace(">", "&gt;")
-        return escape(s)
-
-    def is_packed(self):
-        """
-        Return True if we believe that the AXML file is packed
-        If it is, we can not be sure that the AXML file can be read by a XML Parser
-
-        :return: boolean
-        """
-        return self.axml.packerwarning
-
-    def get_buff(self):
-        return self.buff.encode('utf-8')
-
-    def get_xml(self):
-        """
-        Get the XML as an UTF-8 string
-
-        :return: str
-        """
-        return etree.tostring(self.get_xml_obj(), encoding="utf-8", pretty_print=True)
-
-    def get_xml_obj(self):
-        """
-        Get the XML as an ElementTree object
-
-        :return: :class:`~lxml.etree.Element`
-        """
-        parser = etree.XMLParser(recover=True, resolve_entities=False)
-        tree = etree.fromstring(self.get_buff(), parser=parser)
-        return tree
-
-    def getPrefix(self, prefix):
-        if prefix is None or len(prefix) == 0:
-            return u''
-
-        return prefix + u':'
-
-    def getAttributeValue(self, index):
-        """
-        Wrapper function for format_value
-        to resolve the actual value of an attribute in a tag
-        :param index:
-        :return:
+        :param index: index of the current attribute
+        :return: formatted value
         """
         _type = self.axml.getAttributeValueType(index)
         _data = self.axml.getAttributeValueData(index)
