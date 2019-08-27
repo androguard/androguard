f07e462aaa63126d678b0a511808f5e049671dfe
s <z>
2019-08-27 11:11:02 +0900

    add unittest

---
+++ tests/test_annotations.py
@@ -0,0 +1,12 @@
+import unittest
+import sys
+from androguard.core.bytecodes import dvm
+
+
+class AnnotationTest(unittest.TestCase):
+    def testAnnotation(self):
+        with open("../examples/android/TestsAnnotation/classes.dex", "rb") as f:
+            d = dvm.DalvikVMFormat(f.read())
+            clazz = d.get_class('Landroid/support/v4/widget/SlidingPaneLayout$SlidingPanelLayoutImplJB;')
+            annotations = clazz.get_annotations()
+            self.assertIn('Landroid/support/annotation/RequiresApi;', [clazz.CM.get_type(annotation.type_idx) for annotation in annotations])
