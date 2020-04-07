7f15f82f08d6b8400b4fd3c2ebd813d3e54e98bd
Alex Saveau <saveau.alexandre@gmail.com>
2020-04-07 14:44:25 -0700

    Update apk.py

--- androguard/core/bytecodes/apk.py
+++ androguard/core/bytecodes/apk.py
@@ -1696,12 +1696,7 @@ class APK:
 
         # Test if a signature is found
         if self._APK_SIG_KEY_V2_SIGNATURE in self._v2_blocks:
-            self._
-            
-            
-            
-            
-            gned_v2 = True
+            self._is_signed_v2 = True
 
         if self._APK_SIG_KEY_V3_SIGNATURE in self._v2_blocks:
             self._is_signed_v3 = True
